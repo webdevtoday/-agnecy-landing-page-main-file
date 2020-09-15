dd8eae0819f71abf9131e0631e99e601e20e8159
webdevtoday <coderwd@gmail.com>
2020-09-15 04:00:00 +0300

    Add fonts. Add html-code for First Screen. Create Main-menu and adapt it.

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	const burger = document.querySelector('.burger');
+	const mainMenu = document.querySelector('.main-menu');
+
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('burger_active');
+		mainMenu.classList.toggle('main-menu_active')
+	});
 
 });
